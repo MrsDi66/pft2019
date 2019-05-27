f9d004ba2d2442e66334b0fe988761eb2cb7527a
MrsDi66 <49413543+MrsDi66@users.noreply.github.com>
2019-05-28 00:31:19 +0500

    Заменила наследование на делегирование и создала двухуровневую архитектуру

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+    app.initAddNewContact();
+    app.fillContactForm(new ContactData("Eva", "Semenovna", "Petrova", "Ekaterinburg, Vilonova 6 - 7", "+791290999198", "PetrovaEva@ya.ru"));
+    app.submitNewContact();
+    app.gotoHome();
+  }
+
+}
